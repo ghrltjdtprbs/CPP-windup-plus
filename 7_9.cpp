9d86df8e9d9130cf4b1b21c93da0855a8d90b63c
최혜미 <ghrltjdtprbs@gmail.com>
2022-10-02 15:51:43 +0900

    Add files via upload

--- 7_9.cpp
+++ 7_9.cpp
@@ -2,27 +2,36 @@
 using namespace std;
 class Shape {
 public:
-	Shape(int type, double len) :
-		type_(type), len_(len), PI(3.14) {}
-	~Shape() {}
+	Shape(double type=1,int len=0 ) :
+		type_(type), len_(len), PI(3.14) {
+		if (type == 1)
+			r_count_++;
+		else c_count_++;
+	}
+	
+	~Shape() {
+		r_count_--;
+		c_count_;
+	}
 	double GetArea() {
 		if (type_ == 1)
 			return (len_ * len_);
 		else
-			return (len_ * PI);
+			return (len_*len_*PI );
 	}
-	int GetRectCount() {}
-	int GetCircleCount(){}
+	static int GetRectCount() { return r_count_; }
+	static int GetCircleCount() { return c_count_; }
 
 private: 
 
 	double len_;
     int type_;
 	const double PI;
-	 
+	static int r_count_,c_count_;
 
 };
-
+int Shape::r_count_ = 0;
+int Shape::c_count_ = 0;
 int main() {
 	Shape shape1(1, 5);
 	Shape shape2(2, 5);
