8e15d1845457186907e5d10eb8d4c924b42de7e5
최혜미 <ghrltjdtprbs@gmail.com>
2022-11-01 20:40:54 +0900

    함수에 대한 상속의 문제점 및 해결

--- 17p.cpp
+++ 17p.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
 using namespace std;
 
+#define PI 3.14
 
-int& GetArray(int* ary, int index) { return ary[index]; }
+class Circle {
+public:
+    Circle(int x, int y, double radius) :
+        x_(x), y_(y), radius_(radius) {}
+    double GetArea() { return PI * radius_ * radius_; }
+private:
+    int x_, y_;         // 중심
+    double radius_;     // 반지름
+};
 
+class Sphere : public Circle {
+public:
+    Sphere(int x, int y, int z, double radius) :
+        Circle(x, y, radius), z_(z) {}
+    double GetArea() { return 4 *Circle::GetArea(); }
+    
+private:
+    int z_;
+};
 int main() {
-    int i, ary[5];
+    Sphere sph(1, 1, 1, 1);
 
-    for (i = 0; i < 5; i++)
-        GetArray(ary, i) = i; 
-    for (i = 0; i < 5; i++)
-        cout << "ary[" << i << "] = " << GetArray(ary, i) << endl;
+    cout << sph.GetArea() << endl;
+    
 
     return 0;
 }
