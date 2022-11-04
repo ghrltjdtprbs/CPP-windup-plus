95f7816087bbdbb310dc462865e131f7a9bbf13d
최혜미 <ghrltjdtprbs@gmail.com>
2022-11-04 23:57:31 +0900

    Add files via upload

--- 10_4.cpp
+++ 10_4.cpp
@@ -1,35 +1,33 @@
 #include <iostream>
 using namespace std;
-
 class Date
 {
 public:
-	void Print() {  }
+	void Print() { cout << year_ +" 년 " << month_+" 월 " << day_+" 일 " << endl; }
 protected:
-	int year_ ;
-	int month_ ;
-	int day_ ;
+	int year_=2022;
+	int month_=11;
+	int day_=1;
 
 };
 class Time
 {
 public:
-	void Print() {  }
+	void Print() { cout << hour_ + " 시 " << minute_ + " 분 " << second_ + " 초 " << endl; }
 protected:
-	int hour_ ;
-	int minute_ ;
-	int second_ ;
+	int hour_=20;
+	int minute_=21;
+	int second_=40;
 
 };
-class DateTime : public Date, public Time
+class DateTime : public Date , public Time
 {
 public:
-	void Print() {
-		Date::Print();
-		Time::Print();
-		
+	void Print() { Date::Print(); 
+	Time::Print();
+	cout << "오늘 할일은" + to_do_ << endl;
 	}
 private:
-	char to_do_ ;
+	char to_do_='x';
 
 };
